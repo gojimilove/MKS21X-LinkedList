9e5e3b071d22e575789081b034d337b65555e658
Jessica Kim <jkim00@stuy.edu>
2018-12-13 21:23:29 -0500

    never mind put them both in the same file

---
+++ LinkedList.java
@@ -0,0 +1,37 @@
+public class Node {
+	private int data;
+	private Node next,prev;
+
+	public Node next() {
+
+	}
+	public Node prev() {
+
+	}
+	public void setNext(Node other) {
+
+	}
+	public void setPrev(Node other) {
+
+	}
+	public int getData() {
+
+	}
+	public int setData(int i) {
+
+	}
+	public String toString() {
+
+	}
+
+}
+
+public class MyLinkedList {
+	private int size;
+	private Node start,end;
+
+	public MyLinkedList() {
+
+	}
+	
+}
