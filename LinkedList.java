3223307ec531a9498d6b547510c2ff1cda63765f
Jessica Kim <jkim00@stuy.edu>
2018-12-13 23:44:14 -0500

    set up all methods, started working on some

--- LinkedList.java
+++ LinkedList.java
@@ -1,23 +1,26 @@
 public class Node {
-	private int data;
+	private Integer data;
 	private Node next,prev;
 
+	public Node() {
+		
+	}
 	public Node next() {
-
+		return next;
 	}
 	public Node prev() {
-
+		return prev;
 	}
 	public void setNext(Node other) {
-
+		next = other; 
 	}
 	public void setPrev(Node other) {
+		prev = other;
+	}
+	public Integer getData() {
 
 	}
-	public int getData() {
-
-	}
-	public int setData(int i) {
+	public Integer setData(Integer i) {
 
 	}
 	public String toString() {
@@ -27,10 +30,40 @@ public class Node {
 }
 
 public class MyLinkedList {
-	private int size;
+	private int length;
 	private Node start,end;
 
 	public MyLinkedList() {
+		//create empty list
+	}
+	public boolean add(Integer value) {
+
+	}
+	public int size() {
+
+	}
+	public String toString() {
+
+	}
+	public Integer get(int index) {
+
+	}
+	public Integer set(int index, Integer value) {
+
+	}
+	public boolean contains(Integer value) {
+
+	}
+	public int indexOf(Integer value) {
+
+	}
+	public void add(int index, Integer value) {
+
+	}
+	public Integer remove(int index) {
+
+	}
+	public boolean remove(Integer value) {
 
 	}
 	
