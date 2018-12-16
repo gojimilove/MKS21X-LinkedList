f24e75d56f0c7064ce4ade954eac651d0de78991
Jessica Kim <jkim00@stuy.edu>
2018-12-16 16:13:29 -0500

    changes from class (didnt save)

--- LinkedList.java
+++ LinkedList.java
@@ -2,8 +2,8 @@ public class Node {
 	private Integer data;
 	private Node next,prev;
 
-	public Node() {
-		
+	public Node(Integer x) {
+		data = x;
 	}
 	public Node next() {
 		return next;
@@ -12,19 +12,21 @@ public class Node {
 		return prev;
 	}
 	public void setNext(Node other) {
-		next = other; 
+		next = other;
 	}
 	public void setPrev(Node other) {
 		prev = other;
 	}
 	public Integer getData() {
-
+		return data;
 	}
 	public Integer setData(Integer i) {
-
+		data = i;
 	}
 	public String toString() {
-
+		String result = "";
+		result+= getData();
+		return result;
 	}
 
 }
@@ -34,16 +36,39 @@ public class MyLinkedList {
 	private Node start,end;
 
 	public MyLinkedList() {
-		//create empty list
+		//start and end are both null, point to each other, (empty list) and length = 0
+		start = new Node(null);
+		end = new Node(null);
+		start.setPrev() = null;
+		start.setNext() = end;
+		end.setPrev() = start;
+		end.setNext() = null;
+		length = 0;
 	}
+
 	public boolean add(Integer value) {
+		if (length == 0) { //neither start nor end have a real value yet
+			start.setData() = value;
+		}
+		if (length == 1) { //start has value but not end
+			end.setData() = value;
+		}
+		else { //both start and end have values, must add new one after end
+			Node added = new Node(value);
+			Node before = end.prev();
+			before.setNext() = new Node(end.getData());
 
+
+			end.setNext() = added;
+			added.setPrev() = end;
+			added.setNext() = null;
+		}
+		length++;
+		return true;
 	}
+
 	public int size() {
-
-	}
-	public String toString() {
-
+		return length;
 	}
 	public Integer get(int index) {
 
@@ -66,5 +91,9 @@ public class MyLinkedList {
 	public boolean remove(Integer value) {
 
 	}
-	
+	public String toString() {
+		String result = "[";
+
+	}
+
 }
